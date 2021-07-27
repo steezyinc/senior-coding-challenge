37b907e6782dd1801d2ed5edb72c4d389d6ba3c3
briyams <brianphan@gmail.com>
2021-07-26 19:33:50 -0700

    added app level err catching

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.use(express.json());
 app.use('/users', usersRoutes);
 app.use('/classes', classesRoutes);
 
+// eslint-disable-next-line consistent-return
+app.use((err, req, res, next) => { // generic app level err handling
+  if (err) {
+    return res.status(500).json({ message: 'something went wrong...' });
+  }
+  next();
+});
+
 app.get('/hello', (req, res) => {
   res.json({ hello: 'world' });
 });
